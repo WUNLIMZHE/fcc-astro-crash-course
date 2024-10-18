b05f4f2e679851ecbc87cf16f67c4793c4d9341d
WUN LIM ZHE <wunlimzhe2004@gmail.com>
2024-10-18 10:25:31 +0800

    Added ssr and deploy to netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import mdx from '@astrojs/mdx';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  integrations: [tailwind(), mdx()],
+  output: "server",
+  adapter: netlify()
 });
